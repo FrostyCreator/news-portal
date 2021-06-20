f95d34593441c78d5e1ee9a356a5435f7a775c3e
Salimov Ruslan <salimow.r2015@icloud.com>
2021-06-20 17:39:04 +0500

    Updated configuration reading

--- internal/config/config.go
+++ internal/config/config.go
@@ -18,6 +18,7 @@ type (
 		Host     string
 		User     string
 		Password string
+		URL      string
 		DBName   string `mapstructure:"database"`
 	}
 
@@ -71,6 +72,7 @@ func setConfFromEnvFile(config *Config) error {
 	config.PostgreSQL.Host = viper.GetString("DATABASE_HOST")
 	config.PostgreSQL.User = viper.GetString("DATABASE_USER")
 	config.PostgreSQL.Password = viper.GetString("DATABASE_PASSWORD")
+	config.PostgreSQL.URL = viper.GetString("DATABASE_URL")
 
 	return nil
 }
@@ -79,6 +81,7 @@ func setConfFromEnvVar(config *Config) error {
 	config.PostgreSQL.Host = os.Getenv("DATABASE_HOST")
 	config.PostgreSQL.User = os.Getenv("DATABASE_USER")
 	config.PostgreSQL.Password = os.Getenv("DATABASE_PASSWORD")
+	config.PostgreSQL.URL = viper.GetString("DATABASE_URL")
 
 	return nil
 }
